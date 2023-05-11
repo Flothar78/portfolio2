d2ef4b5fdc29bac9f997079307f7dc22b101691a
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2023-05-11 10:49:27 +0200

    intersectionOserver unobserve when when project pictures on display

--- dist/portfolio.js
+++ dist/portfolio.js
@@ -1,7 +1,12 @@
 "use strict";
 const pictures = document.querySelectorAll(".project-picture");
 const observer = new IntersectionObserver((entries) => {
-    entries.map((entry) => entry.target.classList.toggle("project-picture-animation", entry.isIntersecting));
+    entries.map((entry) => {
+        entry.target.classList.toggle("project-picture-animation", entry.isIntersecting);
+        if (entry.isIntersecting) {
+            observer.unobserve(entry.target);
+        }
+    });
 }, { threshold: 0 });
 pictures.forEach((picture) => {
     observer.observe(picture);
--- src/portfolio.ts
+++ src/portfolio.ts
@@ -1,12 +1,15 @@
 const pictures = document.querySelectorAll(".project-picture");
 const observer = new IntersectionObserver(
   (entries) => {
-    entries.map((entry) =>
+    entries.map((entry) => {
       entry.target.classList.toggle(
         "project-picture-animation",
         entry.isIntersecting
-      )
-    );
+      );
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+      }
+    });
   },
   { threshold: 0 }
 );
